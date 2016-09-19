f0e36f07c723173d5640831f17c54e0a4abc3948
ryhip-ipponusa <ryhip@ipponusa.com>
2016-09-18 23:51:08 -0400

    added view model data and performed initial data binding to html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.imgAttribution = ko.observable('blah');
+
+	this.incrementCounter = function() {};
+}
+
+ko.applyBindings(new ViewModel());
