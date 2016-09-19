1c8a140627e8f4b7af98330977cce057ef37564d
ryhip-ipponusa <ryhip@ipponusa.com>
2016-09-19 00:04:06 -0400

    added nicknames to the cat

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('blah');
+	this.nicknames = ko.observableArray([{name: 'Tubby'}, {name: 'Smartens'}, {name: 'Kiddo'}]);
 
 	this.incrementCounter = function() {};
 }
